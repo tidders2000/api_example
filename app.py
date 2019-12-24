7330e1dee04c745d6f97fab78ccac3145a1f891d
Ubuntu <ubuntu@ip-172-31-95-116.ec2.internal>
2019-12-24 12:53:36 +0000

    added api_all

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask,render_template, redirect, request, url_for, request
+from flask import Flask,render_template, redirect, request, url_for, request, jsonify
+from bson.json_util import dumps
 app = Flask(__name__)
 
 from flask_pymongo import PyMongo
@@ -13,6 +14,12 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("winners.html", 
                            winners=mongo.db.winners.find())
+                           
+@app.route('/api/v1/resources/winners/all', methods=['GET'])
+def api_all():
+    winners=dumps(mongo.db.winners.find())
+    
+    return jsonify(winners)
     
 if __name__=='__main__':
     
